2182ce1b40b9d75d19c08a05109b08b883a50406
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2022-01-30 15:55:45 -0300

    Implements a recursive module to return a list length using the tail call optimization

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: do_call(list, 0)
+
+  defp do_call([], acc), do: acc
+
+  defp do_call([_head | tail], acc) do
+    do_call(tail, acc + 1)
   end
 end
